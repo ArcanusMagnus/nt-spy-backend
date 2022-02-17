494bfb623d1a8923093b3cc7045a6317dc91b528
Pavel Halbich <halbich.p@hotmail.com>
2022-02-17 11:31:26 +0100

    removed dist

--- dist/routes/players.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const players_1 = require("../controllers/players");
-const router = (0, express_1.Router)();
-router.get('/:playerId', players_1.getPlayer);
-router.put('/:playerId', players_1.updatePlayer);
-router.delete('/:playerId', players_1.deletePlayer);
-exports.default = router;
